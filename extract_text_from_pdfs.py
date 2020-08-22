1980233584d2db03c6cba67e10a97c87bdb1169c
Martin Kosík <martin.kosiik@gmail.com>
2020-08-22 18:52:10 +0200

    Update extract_text_from_pdfs.py
    export the resulting df to csv

--- extract_text_from_pdfs.py
+++ extract_text_from_pdfs.py
@@ -24,7 +24,7 @@ for folder in folders_of_pdfs:
     
     for pdf_file in all_pdf_files_in_folder:
         if pdf_file[-8:] ==  ' (1).pdf':
-            os.remove(os.path.join(working_directory, "pdf_files", folder, pdf_file))
+            #os.remove(os.path.join(working_directory, "pdf_files", folder, pdf_file))
             print(pdf_file)
         #print(pdf_file)
     #print(os.path.join(working_directory, folder))
@@ -34,31 +34,34 @@ for folder in folders_of_pdfs:
 # %%
 extracted_text_list = []
 case_id_list = []
+year_list = []
 
 folders_of_pdfs = os.listdir('pdf_files')
 
 for folder in folders_of_pdfs:
     all_pdf_files_in_folder = os.listdir(os.path.join(working_directory, 
                                                       "pdf_files", folder))
-    for pdf_file in all_pdf_files_in_folder[:50]:
+    for pdf_file in all_pdf_files_in_folder:
                     
         with pdfplumber.open(os.path.join("pdf_files", folder, pdf_file)) as pdf:
             all_text = ''
             
             for pdf_page in pdf.pages:
                 single_page_text = pdf_page.extract_text()
-                all_text = all_text + '\n' + single_page_text
+                all_text = all_text + 'NEWPAGE \n' + single_page_text
         
         extracted_text_list.append(all_text)
         case_id_list.append(pdf_file[:-4])   # remove the .pdf characters from the str.
+        year_list.append(int(folder))
+    print('Folder ' + folder + ' finished.')
+        
 
-print(extracted_text_list[3])           
-# %%
-import pdfminer
-print(pdfminer.__version__)  
-text = pdfminer.high_level.extract_text(os.path.join("pdf_files", folder, pdf_file))
-    
 #%%
-print(pdf_file[:-4])
-            
-    
+arbitrage_rulings_df = pd.DataFrame ({'case_id': case_id_list,
+                                    'year': year_list,
+                                    'text': extracted_text_list})
+
+arbitrage_rulings_df.to_csv('arbitrage_rulings.csv', index=False, encoding="UTF-8")      
+# %%
+
+arbitrage_rulings_df = pd.read_csv("arbitrage_rulings.csv", encoding="UTF-8")
