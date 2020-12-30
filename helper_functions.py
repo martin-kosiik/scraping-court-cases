0e5f0163ae6b41d34cf709497280ee483b78ee57
Martin Kosík <martin.kosiik@gmail.com>
2020-12-30 21:11:57 +0100

    ulpoad helper functions

---
+++ helper_functions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def flatten_lists(the_list):
+    #import numpy as np
+    if not isinstance(the_list, list):
+        out_list = the_list
+    elif len(the_list) == 0:
+        out_list = np.nan
+    elif len(the_list) == 1:
+        out_list = the_list[0]
+    else:
+        out_list = the_list
+    return out_list
