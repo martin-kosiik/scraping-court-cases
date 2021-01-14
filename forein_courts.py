04cd7f4f4814bf2ad8bf564f2fe47b92dbd1f845
Martin Kosík <martin.kosiik@gmail.com>
2021-01-14 21:33:33 +0100

    export final_dataset_3.csv

--- forein_courts.py
+++ forein_courts.py
@@ -67,7 +67,7 @@ def reg_match(string, to_match, errs = 1):
 
 any_match = [reg_match(court, '|'.join(tokenized_ukr_regions_flat[1:])) for court in full_text_token_courts]
 
-def match_to_courts(list_of_strings=full_text_token_courts, to_match=tokenized_ukr_regions_flat[1:]):
+def match_to_courts(list_of_strings, to_match):
     court_matches = []
 
     for court in list_of_strings:
@@ -104,7 +104,7 @@ else:
         part_court_match.append(ukr_court)
 
 
-only_courts_list['ukr_court_matches'] = match_to_courts(to_match = ukr_regions.stem_region)
+only_courts_list['ukr_court_matches'] = match_to_courts(list_of_strings=full_text_token_courts, to_match = ukr_regions.stem_region)
 
 
 
@@ -128,11 +128,18 @@ only_courts_list.to_excel('ukr_courts_list_2.xlsx')
 
 # международн коммерческ арбитражн суд при тпп украин
 
-
+# %%
 
 final_dataset = pd.read_csv('final_dataset_2.csv')
 courts_list = pd.read_excel('courts_list.xlsx')
 corrected_courts_list = pd.read_excel('courts_list_manual_VK_OK_final.xlsx')
+corrected_courts_list_colors = pd.read_excel('courts_list_manual_VK_OK_final_copy.xlsx')
+
+courts_list['plaintiff_country_1_not_determ'] = (corrected_courts_list_colors['plaintiff_country_1'] == 'Blue')*1
+courts_list['plaintiff_country_2_not_determ'] = (corrected_courts_list_colors['plaintiff_country_2'] == 'Blue')*1
+courts_list['defendant_country_1_not_determ'] = (corrected_courts_list_colors['defendant_country_1'] == 'Blue')*1
+
+courts_list['backward_claim_switch'] = (corrected_courts_list_colors['proc_text'] == 'Green')*1
 
 assert (courts_list['case_id'] == corrected_courts_list['case_id']).all()
 
@@ -172,6 +179,7 @@ courts_list['court_match_p'] = corrected_courts_list['Court_match_p']
 corrected_courts_list['plaintiff_country_1'][28]
 assert courts_list['plaintiff_country_1'][28] == corrected_courts_list['plaintiff_country_1'][28]
 
+#%%
 
 courts_list_agg = courts_list[['Номер дела', 'courts_in_text', 'ukr_court', 'ukr_court_matches']].groupby('Номер дела', as_index=False).agg({'ukr_court' : [('ukr_court' , 'sum')], 'ukr_court_matches' :[('ukr_court_matches',lambda x:list(x))],
                                                              'courts_in_text': [('courts_in_text', lambda x:list(x))]})
@@ -180,17 +188,13 @@ courts_list_agg.columns = courts_list_agg.columns.get_level_values(1)
 courts_list_agg.rename({'': 'Номер дела'}, axis=1, inplace=True)
 courts_list_agg['ukr_court'].value_counts()
 
+#%%
+
 fl_countries = courts_list[['Номер дела', 'plaintiff_country_1', 'plaintiff_country_2', 'defendat_country_1', 'court_match_d', 'court_match_p']].groupby('Номер дела', as_index=False).agg(list)
 
 
 from helper_functions import unique_list
 
-def unique_list(the_list):
-    if not isinstance(the_list, list):
-        out_list = the_list
-    else:
-        out_list = list(dict.fromkeys(the_list))
-    return out_list
 
 fl_countries = fl_countries.applymap(unique_list).applymap(flatten_lists)
 final_dataset = final_dataset.sort_values('Номер дела')
@@ -203,15 +207,30 @@ final_dataset['defendant_country_1'] = fl_countries['defendat_country_1']
 final_dataset['court_match_d'] = fl_countries['court_match_d']
 final_dataset['court_match_p'] = fl_countries['court_match_p']
 
+#%%
+
+fl_countries_2 = courts_list[['Номер дела', 'plaintiff_country_1_not_determ', 'plaintiff_country_2_not_determ', 'defendant_country_1_not_determ',
+                            'backward_claim_switch']].groupby('Номер дела', as_index=False).agg(list)
+
+fl_countries_2 = fl_countries_2.applymap(unique_list).applymap(flatten_lists)
+
+assert (final_dataset['Номер дела'].reset_index(drop=True) == fl_countries_2['Номер дела']).all()
 
 
+final_dataset['plaintiff_country_1_not_determ'] = fl_countries_2['plaintiff_country_1_not_determ']
+final_dataset['plaintiff_country_2_not_determ'] = fl_countries_2['plaintiff_country_2_not_determ']
+final_dataset['defendant_country_1_not_determ'] = fl_countries_2['defendant_country_1_not_determ']
+final_dataset['backward_claim_switch'] = fl_countries_2['backward_claim_switch']
 
+#%%
+
+final_dataset.columns
 
 final_dataset = pd.merge(final_dataset, courts_list_agg, on = 'Номер дела', how='left')
 
 assert final_dataset.ukr_court.isna().sum() == 0, 'Some cases were not joined'
 
-final_dataset.to_csv('final_dataset_3.csv', index=False, encoding='utf-8')
+final_dataset.to_csv('final_dataset_3_corrected.csv', index=False, encoding='utf-8')
 
 
 # Replace some columns by their manually corrected versions
